da3db0c36c2068bf021ada7c42449b1d50fc808d
Chiara <chiaracardinale92@gmail.com>
2022-12-06 14:53:20 +0100

    first commit

---
+++ resources/views/livewire/comments.blade.php
@@ -0,0 +1,16 @@
+<div class="container-fluid d-flex justify-content-center">
+    
+    <div class="container w-50 px-5">
+        <h1 class="text-center mt-5">Comments</h1>
+        
+        <form>
+            <div class="mb-3">
+              <label for="comment" class="form-label">Comment</label>
+              <input type="text" class="form-control" id="comment" aria-describedby="comment">
+            </div>
+           
+            <button type="submit" class="btn btn-primary">Add comment</button>
+          </form>
+    </div>
+   
+</div>
