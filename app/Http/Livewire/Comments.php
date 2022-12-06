3d60c550d74902f98e3aafec50b786832577b19f
Chiara <chiaracardinale92@gmail.com>
2022-12-06 16:07:04 +0100

    Added: dynamic comments

--- app/Http/Livewire/Comments.php
+++ app/Http/Livewire/Comments.php
@@ -2,10 +2,32 @@
 
 namespace App\Http\Livewire;
 
+use Carbon\Carbon;
 use Livewire\Component;
 
 class Comments extends Component
 {
+    public $newComment;
+
+    public $comments = [
+        [
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam mauris urna, semper quis consequat quis, accumsan non tortor. Vestibulum accumsan diam in ullamcorper tristique. Nam sodales turpis ut tellus faucibus, id pulvinar est accumsan. Etiam lorem mauris, blandit in mollis dictum, rhoncus sit amet erat. Proin euismod a sem euismod volutpat. Donec libero ex, iaculis vitae scelerisque et, consectetur ut elit. Aliquam erat volutpat.',
+            'created_at' => '5 minutes ago',
+            'creator' => 'Chiara',
+        ]
+    ];
+
+    // AddComment -> array_unshift ordina i nuovi commenti dall'alto verso il basso
+    public function addComment(){
+        array_unshift($this->comments, [
+            'body' => $this->newComment,
+            'created_at' => Carbon::now()->diffForHumans(),
+            'creator' => 'Yago',
+        ]);
+
+        $this->newComment = ''; // refresh input text after submit button
+    }
+
     public function render()
     {
         return view('livewire.comments');
--- resources/views/livewire/comments.blade.php
+++ resources/views/livewire/comments.blade.php
@@ -1,16 +1,41 @@
 <div class="container-fluid d-flex justify-content-center">
     
+    {{-- Cointainer --}}
     <div class="container w-50 px-5">
         <h1 class="text-center mt-5">Comments</h1>
         
-        <form>
+        {{-- Form --}}
+        <form class="mt-5">
             <div class="mb-3">
-              <label for="comment" class="form-label">Comment</label>
-              <input type="text" class="form-control" id="comment" aria-describedby="comment">
+              <input type="text" class="form-control" id="comment" placeholder="What's in your mind?" wire:model="newComment">
             </div>
-           
-            <button type="submit" class="btn btn-primary">Add comment</button>
-          </form>
+
+        </form>
+        {{-- /Form --}}
+
+        {{-- Button --}}
+        <div class="d-flex justify-content-end">
+            <button class="btn btn-primary" wire:click="addComment">Add comment</button>
+        </div>
+        {{-- /Button --}}
+
+        <hr class="border border-primary border-2 opacity-25 mt-5">
+
+        {{-- ForEach cards comment --}}
+        @foreach ($comments as $comment)
+            <div class="cards-box mt-5">
+                <div class="card">
+                    {{-- <img src="..." class="card-img-top" alt="..."> --}}
+                    <div class="card-body lh-1">
+                        <p class="card-text text-muted fw-light">{{$comment['created_at']}} by <b>{{$comment['creator']}}</b></p>
+                        <p class="card-text">{{$comment['body']}}</p>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+        {{-- /ForEach cards comment --}}
+
     </div>
+    {{-- /Cointainer --}}
    
 </div>
